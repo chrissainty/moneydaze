b468528b057d78e4cb17082fb93c2d286f42301f
Chris Sainty <chrissainty@users.noreply.github.com>
2018-04-24 21:00:14 +0100

    added delete ability for incomes and outgoings

--- MoneyDaze/Services/Outgoing.cs
+++ MoneyDaze/Services/Outgoing.cs
@@ -1,9 +1,18 @@
-﻿namespace MoneyDaze.Services
+﻿using System;
+
+namespace MoneyDaze.Services
 {
     public class Outgoing
     {
+        public Guid Id { get; set; }
         public string Description { get; set; }
         public Category Category { get; set; }
-        public decimal Amount { get; set; }
+        public string Amount { get; set; }
+
+        public Outgoing()
+        {
+            Id = Guid.NewGuid();
+        }
+
     }
 }
